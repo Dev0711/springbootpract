b2204c617d22cb6c47ca4ec4b95eef4b0a2b6252
Dev0711 <111134895+Dev0711@users.noreply.github.com>
2026-03-26 23:50:33 +0530

    product repository added

--- backend/product-service/src/main/java/com/springpract/product_service/service/Impl/ProductServiceImpl.java
+++ backend/product-service/src/main/java/com/springpract/product_service/service/Impl/ProductServiceImpl.java
@@ -1,6 +1,31 @@
 package com.springpract.product_service.service.Impl;
 
+import com.springpract.product_service.dto.ProductRequest;
+import com.springpract.product_service.dto.ProductResponse;
+import com.springpract.product_service.entity.Product;
+import com.springpract.product_service.repository.ProductRepository;
 import com.springpract.product_service.service.ProductService;
 
+import java.util.List;
+import java.util.UUID;
+
 public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+    @Override
+    public Product getProductById(UUID id) {
+        Product product = productRepository.findById(id).orElseThrow(()-> new RuntimeException("Product not found"));
+        return product;
+
+
+    }
+
+    @Override
+    public List<Product> getProductByCategory(String productCategory) {
+         List<Product> product = productRepository.findByProductCategory(productCategory);
+         return product;
+
+    }
+
+
 }
--- backend/product-service/src/main/java/com/springpract/product_service/service/ProductService.java
+++ backend/product-service/src/main/java/com/springpract/product_service/service/ProductService.java
@@ -1,9 +1,19 @@
 package com.springpract.product_service.service;
 
+import com.springpract.product_service.dto.ProductRequest;
+import com.springpract.product_service.entity.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface ProductService {
+
+    Product getProductById(UUID id);
+
+    List<Product> getProductByCategory(String productCategory);
+
 
 
 }
